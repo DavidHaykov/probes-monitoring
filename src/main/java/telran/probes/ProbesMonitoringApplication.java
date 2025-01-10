53b7949d0c69bea8c43c0a60d4e3dcca61b29f7d
DavidHaykov <mr.haykov@icloud.com>
2025-01-10 09:54:33 +0200

    1

--- src/main/java/telran/probes/ProbesMonitoringApplication.java
+++ src/main/java/telran/probes/ProbesMonitoringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProbesMonitoringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProbesMonitoringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProbesMonitoringApplication.class, args);
+    }
 
 }
